73a42fe0ea6b11411a15eb0059df61ccb4145242
sj <jainsanmati846@gmail.com>
2019-07-08 20:39:50 +0530

    functionality to clear and check input fields

--- app.js
+++ app.js
@@ -100,6 +100,22 @@ var UIController = (function() {
       newHtml = newHtml.replace('%value%', obj.value);
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ', ' + DOMstrings.inputValue
+      );
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = '';
+      });
+
+      fieldsArr[0].focus();
     }
   };
 })();
@@ -118,16 +134,29 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function() {
+    // 1. Calculate the budget
+    // 2. Return the budget
+    // 3. Display the budget
+  };
+
   var ctrlAddItem = function() {
     var input, newItem;
     // 1. Get the feild input data
     input = UICtrl.getInput();
-    // 2. Add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    // 3. Add the item to the ui
-    UICtrl.addListItem(newItem, input.type);
-    // 4. Calculate the budget
-    // 5. Display the budget
+
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3. Add the item to the ui
+      UICtrl.addListItem(newItem, input.type);
+      // 4. Clear the fields
+      UICtrl.clearFields();
+      // 5. Calculate and update the budget
+      updateBudget();
+    } else {
+      console.log('dshsj');
+    }
   };
 
   return {
